dafb6bab19e7bd1d73df37606c8dc9e81827ee0a
Jude Ning <Jude.Ning@signify.co.nz>
2023-05-10 14:34:38 +1200

    feat: add a custom dropdown field tailored for selecting AWS S3 videos.

---
+++ src/Forms/S3DropdownField.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Signify\Forms;
+
+use SilverStripe\Forms\DropdownField;
+use Signify\Models\S3Video;
+
+/**
+ * Dropdown selection for {@link Signify\Models\S3Video} data objects with 
+ * a link in the description to the video admin section of
+ * the CMS and an empty string option pre-set.
+ */
+class S3DropdownField extends DropdownField
+{
+    public function __construct(
+        $name,
+        $title = null,
+        $source = null,
+        $value = "",
+    ) {
+        $source = $source ?? S3Video::get()->map('ID', 'Name')->toArray();
+                
+        $this->setDescription(
+            '<em><a href="admin/videos/" target="__blank">'
+            . 'Add or edit videos '
+            . '<span class="font-icon-external-link"></a><em>'
+        ); 
+        $this->setEmptyString('--Select Video--');
+
+        parent::__construct($name, $title, $source, $value);
+    }
+}
