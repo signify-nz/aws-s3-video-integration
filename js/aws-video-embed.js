cd660d1e960ed753c3ff94809931d99872129223
Peggy Nobes <peggy.nobes@signify.co.nz>
2023-05-16 08:43:37 +1200

    fix: correctly update value in React input on dropdown change MOJ045-340
    Text field stopped working if dropdown had been changed, and then changed back
    Use property and prototype setters to update field value correctly

--- js/aws-video-embed.js
+++ js/aws-video-embed.js
@@ -1,15 +1,28 @@
 (function($) {
   $.entwine('ss', function($) {
 
+    // Set value for React object
+    const setNativeValue = (element, value) => {
+      const valueSetter = Object.getOwnPropertyDescriptor(element, 'value').set;
+      const prototype = Object.getPrototypeOf(element);
+      const prototypeValueSetter = Object.getOwnPropertyDescriptor(prototype, 'value').set;
+
+      if (valueSetter && valueSetter !== prototypeValueSetter) {
+          prototypeValueSetter.call(element, value);
+      } else {
+          valueSetter.call(element, value);
+      }
+    };
+
     $('select.js-aws-video-field').entwine({
       onchange: function() {
           let $urlField = $('input.js-url-embed-field');
           let target = $urlField[0];
           if (this.context.value) {
-            Object.defineProperty(target, 'value', {value: this.context.value});
+            setNativeValue(target, this.context.value);
             $urlField.attr('disabled', true);
           } else {
-            Object.defineProperty(target, 'value', {value: ''});
+            setNativeValue(target, '');
             $urlField.attr('disabled', false);
           }
           const event = new Event("input", {bubbles: true});
