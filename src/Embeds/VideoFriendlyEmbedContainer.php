f96adc6b59e95e67a229e3aee8eacf0099b4ee7f
Levi Wilson-Brown <levi.wilson-brown@signify.co.nz>
2025-07-25 00:47:06 +1200

    feat: Improved handling of video embeds within TinyMCE. #4
    - S3 buckets can be dynamically added to 'domains_excluded_from_sandboxing'.
    - Direct files (mp4, etc) will no longer request the whole video when inserting via TinyMCE 'Insert media via URL' button (or when loading uncached page).
     -- A head check is still done on the file, to at least ensure something exists.
    - Nicer (but still static) thumbnail for videos in TinyMCE.
    - Videos will have a more appropriate default width/height.

---
+++ src/Embeds/VideoFriendlyEmbedContainer.php
@@ -0,0 +1,150 @@
+<?php
+
+namespace Signify\Embeds;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+use SilverStripe\Core\Config\Configurable;
+use SilverStripe\Core\Manifest\ModuleResourceLoader;
+use SilverStripe\View\Embed\EmbedContainer;
+
+/**
+ * Embed container that better supports direct video files (mp4, etc).
+ */
+class VideoFriendlyEmbedContainer extends EmbedContainer
+{
+    use Configurable;
+
+    /**
+     * Default width for direct video embeds.
+     *
+     * @config
+     * @var int
+     */
+    private static int $direct_video_width = 640;
+
+    /**
+     * Aspect ratio height (numerator) for video.
+     *
+     * @config
+     * @var int
+     */
+    private static int $aspect_ratio_height = 9;
+
+    /**
+     * Aspect ratio width (denominator) for video.
+     *
+     * @config
+     * @var int
+     */
+    private static int $aspect_ratio_width = 16;
+
+    /**
+     * @var string
+     */
+    protected string $embedUrl;
+
+    public function __construct(string $url)
+    {
+        // $url property in parent is private in the parent class so we cannot use it here.
+        // So copy the value as 'embedUrl'
+        $this->embedUrl = $url;
+        parent::__construct($url);
+    }
+
+    /**
+     * Use a more suitable height for direct video files.
+     */
+    public function getHeight(): int
+    {
+        if ($this->isDirectVideo()) {
+            $width = $this->getWidth();
+            $heightRatio = self::config()->get('aspect_ratio_height');
+            $widthRatio = self::config()->get('aspect_ratio_width');
+            return (int)($width * $heightRatio / $widthRatio);
+        }
+
+        return parent::getHeight();
+    }
+
+    /**
+     * Use a more suitable width for direct video files.
+     */
+    public function getWidth(): int
+    {
+        if ($this->isDirectVideo()) {
+            return self::config()->get('direct_video_width');
+        }
+
+        return parent::getWidth();
+    }
+
+    /**
+     * Show nicer video placeholder image for preview/placeholder within TinyMCE.
+     */
+    public function getPreviewURL(): string
+    {
+        if ($this->isDirectVideo()) {
+            return ModuleResourceLoader::resourceURL(
+                'signify-nz/aws-s3-video-integration:images/icon_video.png'
+            );
+        }
+
+        return parent::getPreviewURL();
+    }
+
+    /**
+     * Get embed name
+     */
+    public function getName(): string
+    {
+        if ($this->isDirectVideo()) {
+            return basename($this->embedUrl);
+        }
+        return parent::getName();
+    }
+
+    /**
+     * Direct
+     */
+    public function getType(): string
+    {
+        if ($this->isDirectVideo()) {
+            return 'video';
+        }
+
+        return parent::getType();
+    }
+
+    /**
+     * Validate embed.
+     */
+    public function validate(): bool
+    {
+        // If direct video, check that it exists.
+        if ($this->isDirectVideo()) {
+            $client = new Client([
+                'timeout' => 10,  // seconds
+                'allow_redirects' => true,
+                'http_errors' => false,  // Don’t throw exceptions on 4xx/5xx status
+            ]);
+
+            try {
+                $response = $client->head($this->embedUrl);
+                // Return true only if status code is 200 OK
+                return $response->getStatusCode() === 200;
+            } catch (GuzzleException $e) {
+                return false;
+            }
+        }
+        return parent::validate();
+    }
+
+    /**
+     * Check whether the embed is a direct video.
+     */
+    public function isDirectVideo(): bool
+    {
+        return preg_match('/\.(mp4|webm|ogg)$/i', $this->embedUrl) === 1;
+    }
+}
---
+++ src/Views/Shortcodes/VideoFriendlyEmbedShortcodeProvider.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace Signify\Views\Shortcodes;
+
+use Psr\SimpleCache\CacheInterface;
+use Signify\Models\S3Bucket;
+use SilverStripe\Core\Config\Config;
+use SilverStripe\Core\Config\Configurable;
+use SilverStripe\Core\Flushable;
+use SilverStripe\Core\Injector\Injector;
+use SilverStripe\View\Embed\Embeddable;
+use SilverStripe\View\Shortcodes\EmbedShortcodeProvider;
+
+/**
+ * Extends the default embed shortcode provider to avoid network requests to direct video files.
+ *
+ * Without this, memory issues are hit when embedding videos of a reasonable size,
+ * due to calls to $embeddable->getExtractor();
+ */
+class VideoFriendlyEmbedShortcodeProvider extends EmbedShortcodeProvider implements Flushable
+{
+    use Configurable;
+
+   /**
+     * Cache time-to-live (TTL) in seconds for video domain caching.
+     *
+     * This controls how long the list of video domains (e.g. S3 buckets)
+     * is cached before being refreshed.
+     *
+     * @config
+     * @var int
+     */
+    private static int $domains_ttl = 300;
+
+    /**
+     * Override shortcode handler
+     *
+     * {@inheritdoc}
+     */
+    public static function handle_shortcode($arguments, $content, $parser, $shortcode, $extra = []): string
+    {
+        // Get service URL
+        if (!empty($content)) {
+            $serviceURL = $content;
+        } elseif (!empty($arguments['url'])) {
+            $serviceURL = $arguments['url'];
+        } else {
+            return '';
+        }
+
+        $embeddable = Injector::inst()->create(Embeddable::class, $serviceURL);
+
+        if (method_exists($embeddable, 'isDirectVideo') && $embeddable->isDirectVideo()) {
+            self::setExtractorUrl($serviceURL);
+
+            self::updateDomainsExcludedFromSandboxing();
+            $safeUrl = htmlspecialchars($serviceURL, ENT_QUOTES);
+            return static::videoEmbed($arguments, "<video controls src='{$safeUrl}' controlsList='nodownload'></video>");
+        }
+
+        return parent::handle_shortcode($arguments, $content, $parser, $shortcode, $extra);
+    }
+
+    /**
+     * Ensure flushing the cache clears videoEmbeds cache.
+     */
+    public static function flush(): void
+    {
+        Injector::inst()->get(CacheInterface::class . '.videoEmbeds')->clear();
+    }
+
+    /**
+     * Allow S3 bucket domains to be excluded from sandboxing.
+     *
+     * IE. Do not wrap with iFrame.
+     */
+    protected static function updateDomainsExcludedFromSandboxing(): void
+    {
+        $cache = Injector::inst()->get(CacheInterface::class . '.videoEmbeds');
+        $cachedDomains = $cache->get('videoDomains');
+        if ($cachedDomains !== null) {
+            $domains = $cachedDomains;
+        } else {
+            $s3buckets = S3Bucket::get();
+            $domains = [];
+            foreach ($s3buckets as $s3Bucket) {
+                $domains[] = parse_url($s3Bucket->Domain, PHP_URL_HOST);
+            }
+            $ttl = static::config()->get('domains_ttl');
+            $cache->set('videoDomains', $domains, $ttl);
+        }
+        Config::modify()->merge(EmbedShortcodeProvider::class, 'domains_excluded_from_sandboxing', $domains);
+    }
+
+    /**
+     * Set the extractorUrl property.
+     *
+     * ReflectionClass is needed as $extractorUrl is a private property in the parent class.
+     *
+     * @param string $url The URL to set as the extractor URL.
+     * This is the URL that will be checked for the sandboxing check.
+     */
+    protected static function setExtractorUrl(string $url): void
+    {
+        $reflection = new \ReflectionClass(get_parent_class(static::class));
+        $property = $reflection->getProperty('extractorUrl');
+        $property->setAccessible(true);
+        $property->setValue(null, $url);
+    }
+}
